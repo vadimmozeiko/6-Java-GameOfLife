bf2cbc3ef07ab6c1c5698b13fa01a71bdc15453a
Vadim <vadim.mozeiko@gmail.com>
2021-03-13 14:52:32 +0200

    commit

--- src/nd/pkg6/ND6.java
+++ src/nd/pkg6/ND6.java
@@ -4,6 +4,7 @@ public class ND6 {
 
     public static void main(String[] args) {
 
+        // Generates random board
         char[][] field = new char[10][20];
         for (int y = 0; y < field.length; y++) {
             char[] line = field[y];
@@ -15,7 +16,8 @@ public class ND6 {
                 }
             }
         }
-        
+
+        // Prints out current generated board
         for (int y = 0; y < field.length; y++) {
             char[] line = field[y];
             for (int x = 0; x < line.length; x++) {
@@ -25,29 +27,15 @@ public class ND6 {
         }
         System.out.println("--------------------");
 
-        
-        int width = field.length;
-        int height = field[0].length;
-        // Create a copy of the original board
-        char[][] copyBoard = new char[width][height];
-
-        // Create a copy of the original board
-        for (int row = 0; row < width; row++) {
-            for (int col = 0; col < height; col++) {
-                copyBoard[row][col] = field[row][col];
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
+        // Generates 50 iterations
+        for (int i = 0; i < 1; i++) {
+            
             for (int j = 0; j < field.length; j++) {
-                char[] line = field[j];
-                for (int x = 0; x < line.length; x++) {
-                    System.out.print(line[x]);
-                }
-                System.out.println();
+                for (int k = 0; k < field[i].length; k++) {
+          
             }
-            System.out.println("====================");
         }
 
     }
+}
 }

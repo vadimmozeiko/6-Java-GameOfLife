f353296c8ec0962cf0237051abe2bf220410c8dc
Vadim <vadim.mozeiko@gmail.com>
2021-03-13 10:39:58 +0200

    board copy

--- src/nd/pkg6/ND6.java
+++ src/nd/pkg6/ND6.java
@@ -1,12 +1,10 @@
-
 package nd.pkg6;
 
 public class ND6 {
 
     public static void main(String[] args) {
-     
-        
-        char[][] field = new char[50][50];
+
+        char[][] field = new char[10][20];
         for (int y = 0; y < field.length; y++) {
             char[] line = field[y];
             for (int x = 0; x < line.length; x++) {
@@ -17,7 +15,7 @@ public class ND6 {
                 }
             }
         }
-
+        
         for (int y = 0; y < field.length; y++) {
             char[] line = field[y];
             for (int x = 0; x < line.length; x++) {
@@ -26,7 +24,30 @@ public class ND6 {
             System.out.println();
         }
         System.out.println("--------------------");
+
         
+        int width = field.length;
+        int height = field[0].length;
+        // Create a copy of the original board
+        char[][] copyBoard = new char[width][height];
+
+        // Create a copy of the original board
+        for (int row = 0; row < width; row++) {
+            for (int col = 0; col < height; col++) {
+                copyBoard[row][col] = field[row][col];
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < field.length; j++) {
+                char[] line = field[j];
+                for (int x = 0; x < line.length; x++) {
+                    System.out.print(line[x]);
+                }
+                System.out.println();
+            }
+            System.out.println("====================");
+        }
+
     }
-    
 }

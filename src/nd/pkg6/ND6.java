b09a43a1afb64898b9642c055d02c3d7504f4b7d
Vadim <vadim.mozeiko@gmail.com>
2021-03-13 10:16:38 +0200

    generate board

--- src/nd/pkg6/ND6.java
+++ src/nd/pkg6/ND6.java
@@ -5,6 +5,28 @@ public class ND6 {
 
     public static void main(String[] args) {
      
+        
+        char[][] field = new char[50][50];
+        for (int y = 0; y < field.length; y++) {
+            char[] line = field[y];
+            for (int x = 0; x < line.length; x++) {
+                if (Math.random() < 0.2) {
+                    line[x] = 'X';
+                } else {
+                    line[x] = '.';
+                }
+            }
+        }
+
+        for (int y = 0; y < field.length; y++) {
+            char[] line = field[y];
+            for (int x = 0; x < line.length; x++) {
+                System.out.print(line[x]);
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------");
+        
     }
     
 }
